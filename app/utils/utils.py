e47b9f4802eed22689d806c8cb9ba6600d09d4da
Ratolon <miceta@cnb.csic.es>
2025-11-06 13:03:16 +0100

    Last padding adjustment

--- app/utils/utils.py
+++ app/utils/utils.py
@@ -9,8 +9,8 @@ def estimate_table_height(df):
     if df is None or df.empty:
         return 128  # very little in case it's empty
     
-    row_height = 24 # Default row height
-    base_padding = 50  # Base padding
+    row_height = 32 # Default row height
+    base_padding = 80  # Base padding
 
     height = len(df) * row_height + base_padding
     height = max(height, 200) # At least 200px always
